6648dd42266e26fb43024bad7f9515ecf8a5d7a9
Umar Ejaz <johndoe@example.com~>
2024-08-12 07:45:39 -0700

    finale

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 function App() {
+  const [obj, setobj] = useState({ name: "umar", ban: false });
+  // const [score, setscore] = useState(1);
+
   return (
-    <h1>hello world</h1>
+    <>
+      <h1>Name : {obj.name} </h1>
+      <h1>ban : {obj.ban.toString()} </h1>
+
+      <button
+        className={`px-4 py-2 ${obj.ban ? "bg-red-500" : "bg-blue-500"}`}
+        onClick={() => setobj({ ...obj, ban: !obj.ban })}
+      >
+        update
+      </button>
+    </>
   );
 }
 
